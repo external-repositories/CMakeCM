4c46626ebd323d3a80a5f24ac9629e50c1317334
flagarde <flagarde@users.noreply.github.com>
2020-05-05 03:43:01 +0800

    Update CMakeCM.cmake

--- CMakeCM.cmake
+++ CMakeCM.cmake
@@ -8,7 +8,7 @@ macro(_cmcm_set_if_undef varname)
 endmacro()
 
 # This is the base URL to resolve `LOCAL` modules
-_cmcm_set_if_undef(CMCM_LOCAL_RESOLVE_URL "https://github.com/flagarde/CMakeCM/blob/master")
+_cmcm_set_if_undef(CMCM_LOCAL_RESOLVE_URL "https://raw.githubusercontent.com/flagarde/CMakeCM/master/")
 # This is the directory where CMakeCM will store its downloaded modules
 _cmcm_set_if_undef(CMCM_MODULE_DIR "${CMAKE_BINARY_DIR}/_cmcm-modules")
 
